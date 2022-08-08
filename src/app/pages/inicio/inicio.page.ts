fba80c47be868a742ab168c938be57454b5d84c7
gquinteros-cerveros <gquinteros@cerveros.cl>
2022-08-08 01:17:07 -0400

    list

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Conmponente {
+  icon: string,
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+componentes: Conmponente[] = [
+  {
+    icon: 'add-circle-outline',
+    name: 'Action sheet',
+    redirectTo: '/action-sheet'
+  },
+  {
+    icon: 'warning',
+    name: 'Alert',
+    redirectTo: '/alert'
+  }
+];
 
   constructor() { }
 
